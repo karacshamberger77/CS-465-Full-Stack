82660ae04ed004df135b4dd60d1732cf95f7c2bd
karacshamberger77 <shambergerk@gmail.com>
2022-01-21 13:04:49 -0500

    Convert travel from static to dynamic JSON

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,8 +1,12 @@
+const fs = require('fs');
+const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
+
 /* GET travel view */
 const travel = (req,res)=>{
-    res.render('travel',{title: 'Travlr Gateways'});
+    res.render('travel',{title: 'Travlr Gateways', trips});
 };
-
+ 
 module.exports = {
     travel
 };
